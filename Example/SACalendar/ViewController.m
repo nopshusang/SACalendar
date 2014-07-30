98fec4ce6ff981322c9d7e0741382e12ab4ef6f2
Nop Shusang <guttit@hotmail.com>
2014-07-29 23:15:27 -0400

    Simplify the sample code
    - use vertical scrolling, to match the iOS Calendar app
    - use print statements to demonstrate the delegate
    - update the screenshot

--- Example/SACalendar/ViewController.m
+++ Example/SACalendar/ViewController.m
@@ -13,9 +13,6 @@
 
 @interface ViewController () <SACalendarDelegate>
 
-@property (strong, nonatomic) IBOutlet UILabel *dateSelectedLabel;
-@property (strong, nonatomic) IBOutlet UILabel *displayLabel;
-
 @end
 
 @implementation ViewController
@@ -24,23 +21,37 @@
 {
     [super viewDidLoad];
 	
-    SACalendar *calendar = [[SACalendar alloc]initWithFrame:CGRectMake(0, 20, 320, 400)
-                                            scrollDirection:ScrollDirectionHorizontal
-                                              pagingEnabled:YES];
+    /*
+     * Smooth scrolling in vertical direction
+     * - to change to horizontal, change the scrollDirection to ScrollDirectionHorizontal
+     * - to use paging scrolling, change pagingEnabled to YES
+     * - to change the looks, please see documentation on Github
+     * - the calendar works with any size
+     */
+    SACalendar *calendar = [[SACalendar alloc]initWithFrame:CGRectMake(0, 20, 320, 500)
+                                            scrollDirection:ScrollDirectionVertical
+                                              pagingEnabled:NO];
+    
+
     
     calendar.delegate = self;
     
     [self.view addSubview:calendar];
 }
 
+/**
+ *  Delegate method : get called when a date is selected
+ */
 -(void) SACalendar:(SACalendar*)calendar didSelectDate:(int)day month:(int)month year:(int)year
 {
-    _dateSelectedLabel.text = [NSString stringWithFormat:@"Date Selected :      %02i/%02i/%04i",day,month,year];
+    NSLog(@"Date Selected : %02i/%02i/%04i",day,month,year);
 }
 
+/**
+ *  Delegate method : get called user has scroll to a new month
+ */
 -(void) SACalendar:(SACalendar *)calendar didDisplayCalendarForMonth:(int)month year:(int)year{
-    _dateSelectedLabel.text = @"Date Selected : ";
-    _displayLabel.text = [NSString stringWithFormat:@"Displaying :            %@ %04i",[DateUtil getMonthString:month],year];
+    NSLog(@"Displaying : %@ %04i",[DateUtil getMonthString:month],year);
 }
 
 @end
